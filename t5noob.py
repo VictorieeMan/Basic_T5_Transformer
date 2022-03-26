1264796965c0fc96887fc29b2f01f5bb65ef23db
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-03-26 10:20:12 +0100

    Got a working t5-train cykle running
    Based on this tutorial: https://huggingface.co/docs/transformers/model_doc/t5
    
    No inference implemented yet to judge the results

--- t5noob.py
+++ t5noob.py
@@ -11,6 +11,9 @@ try:
     import matplotlib.pyplot as plt
 
     import torch
+    import transformers
+
+    from transformers import AutoTokenizer, AutoModelWithLMHead
 
     from tqdm import tqdm # Progressbar
     # *https://www.pythonpool.com/python-tqdm/
@@ -19,3 +22,42 @@ except Exception as e:
     raise Exception("Couldn't load all modules. More info: {}".format(e))
 
 print("Gooday ax shaft!")
+
+tokenizer = AutoTokenizer.from_pretrained("t5-small")
+model = AutoModelWithLMHead.from_pretrained("t5-small")
+
+#Hyperparameters
+max_source_len = 512
+max_target_len = 128
+
+#Training examples
+input_seq_1 = "There's a monkey under the table."
+output_seq_1 = "Det är en apa under bordet."
+
+input_seq_2 = "Why do pigs fly?"
+output_seq_2 = "Varför flyger grisar?"
+
+#Encoding the inputs
+task_prefix = "Translate Eng to Swe"
+input_sequences = [input_seq_1,input_seq_2]
+encoding = tokenizer(
+    [task_prefix+ seq for seq in input_sequences],
+    padding = "longest",
+    max_length=max_source_len,
+    truncation=True,
+    return_tensors="pt"
+)
+input_ids, attention_mask = encoding.input_ids, encoding.attention_mask
+
+#Encoding targets
+target_encoding = tokenizer(
+    [output_seq_1, output_seq_2], padding="longest", max_length=max_target_len, truncation=True
+)
+labels = target_encoding.input_ids
+
+#Replace padding token id's of the labels
+labels = torch.tensor(labels)
+labels[labels == tokenizer.pad_token_id] = -100
+
+# forward pass
+loss = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels).loss
